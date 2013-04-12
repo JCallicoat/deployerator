61a2e003dc0269b5dd9ecfa3cf0a5b00c77de874
JCallicoat <jordan.callicoat@rackspace.com>
2013-04-12 00:36:03 -0500

    make core_get_servers.py better

--- core_get_servers.py
+++ core_get_servers.py
@@ -1,7 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
 from core_helper import CoreHelper
+
+if len(sys.argv) < 2:
+    prog = os.path.basename(sys.argv[0])
+    sys.exit('usage: {} ACCOUNT_NUMBER'.format(prog))
 
 ch = CoreHelper()
 computers = ch.get_stack_servers(sys.argv[1:])
 
-with open('/tmp/deploy.json', 'w') as fh:
-  fh.write(json.dumps(computers))
+print('Writing server list from CORE to /tmp/deploy.json')
+
+try:
+    with open('/tmp/deploy.json', 'w') as fh:
+        data = json.dumps(computers, indent=4)
+        fh.write(data)
+        print(data)
+except Exception as e:
+    print('ARGGGGGGGGGGGGG! {}'.format(e.message))
+else:
+    print('OK, bye!')
